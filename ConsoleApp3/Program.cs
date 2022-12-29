c02951f021a78535d71000bbfc6009cec3b9ea39
Swapnil Santosh Muluk <swapnilmuluk6@gmail.com>
2022-12-29 14:42:23 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            //string name = "swapnil";
+            //int age = 22;
+            //string city = "pune";
+
+            //Console.WriteLine($"My name is {name},my age is {age}, and i'm living in {city}.");
+            // Console.WriteLine("My name is " + name + " my age is " + age + " i am living in " + city+".");
+            //Console.WriteLine("my name is {0} my age is {1}.", name, age);
+
+
+            //Console.WriteLine("Enter you name");
+            //string name= Console.ReadLine();
+
+            // Console.WriteLine("Enter your age");
+            //int age=int.Parse(Console.ReadLine());
+            //or
+            //int age2=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter Percentage");
+            //double percentage=Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine($" My name is {name} , age is {age} $ {age2} percentage={percentage}");
+
+
+            //Console.WriteLine("Enter Radius");
+            //double radius=Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Area of Circle");
+            //double Area_of_Circle = 3.14 * radius * radius;
+
+            //Console.WriteLine(Area_of_Circle);
+
+            //Console.WriteLine("For Area of Rectangle");
+            //Console.WriteLine("Enter Length");
+            //double length=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter Breadth");
+            //double breadth=Convert.ToDouble(Console.ReadLine());
+
+            //double Area_of_Rectangle = length * breadth;
+            //Console.WriteLine("Area_of_Rectangle");
+            //Console.WriteLine(Area_of_Rectangle);
+
+            //Console.WriteLine("For cube and Square");
+            //Console.WriteLine("Enter Number");
+            //double number = Convert.ToDouble(Console.ReadLine());
+
+
+
+            //double Square_of_Number = number * number;
+            //Console.WriteLine("Square of Number",Square_of_Number);
+
+            //double Cube_of_number=number*number*number;
+            //Console.WriteLine("Cube of Number",Cube_of_number);
+
+
+            //Console.WriteLine("Enter EMP ID");
+           // int id=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter EMP NAME");
+            //string name=Console.ReadLine();
+            //Console.WriteLine("Enter Your Salary");
+            //double salary= Convert.ToDouble(Console.ReadLine());
+
+          
+           // double hra = (salary / 100) * 12;
+            //Console.WriteLine($"hra :{hra}");
+            //double da = (salary / 100) * 20;
+            ///Console.WriteLine($"da : {da}");
+            //double ta = (salary / 100) * 10;
+            //Console.WriteLine($"ta :{ta}");
+            //double pf = (salary / 100) * 12;
+            //Console.WriteLine($"pf : {pf}");
+           // Console.WriteLine("Total salary");
+            //double ts = salary+hra+da+ta-pf;
+           // Console.WriteLine(ts);
+
+            Console.WriteLine("Enter 1st Number");
+            double A=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second NUmber");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            double c = A;
+            A = B;
+            B = c;
+            Console.WriteLine($"A: {A}");
+            Console.WriteLine($"B:{B}");
+            
+
+                     
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp3/deomproject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class deomproject
+    {
+        static void Main1(string[] args)
+        {
+            //Console.WriteLine("Enter Number");
+            //int num=Convert.ToInt32(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("It is Even Number");
+
+            //}
+            // else
+            // {
+            //    Console.WriteLine("IT is Odd Number");
+            // }
+            Console.WriteLine("Enter NUmber");
+            int num=Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("It is positive");
+
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("It is zero");
+
+            }
+            else
+            {
+                Console.WriteLine("it is negatiove");
+            }
+
+        }
+    }
+}
